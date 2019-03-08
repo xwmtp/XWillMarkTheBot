13409f75100778ecd7e68e9feb5d1be22d09640f
xwmtp <sdstempher@ziggo.nl>
2019-03-08 22:45:31 +0100

    Added !entrants command

--- xwillmarktheBot/Message_handlers/SRL_races/Race_handler.py
+++ xwillmarktheBot/Message_handlers/SRL_races/Race_handler.py
@@ -1,6 +1,7 @@
 from xwillmarktheBot.Message_handlers.Message_handler import Message_handler
 from xwillmarktheBot.Message_handlers.SRL_races.SRL_races import SRL
 from xwillmarktheBot import Settings
+from xwillmarktheBot.Utils import *
 
 class Race_handler(Message_handler):
 
@@ -11,7 +12,8 @@ class Race_handler(Message_handler):
         self.commands = {
             'race' : ['!race'],
             'card' : ['!card', '!board', '!chart'],
-            'goal' : ['!goal']
+            'goal' : ['!goal'],
+            'entrants' : ['!entrants']
         }
 
         self.SRL = SRL()
@@ -27,8 +29,13 @@ class Race_handler(Message_handler):
             return self.send("No SRL race found.")
 
         # current race
-        if command in self.commands['race'] + self.commands['goal'] + self.commands['card'] :
+        if command in self.current_race_commands():
             self.send(self.get_current_race_info(command))
+
+
+    def current_race_commands(self):
+        current_race_groups = ['race', 'goal', 'card', 'entrants']
+        return flatten([self.commands[group] for group in current_race_groups])
 
 
 
@@ -43,10 +50,10 @@ class Race_handler(Message_handler):
         elif command in self.commands['race']:
             answer = self.SRL.current_race.get_race_link()
 
-        if command in self.commands['race']:
+        if (command in self.commands['entrants']) | (Settings.PRINT_ENTRANTS & (command in self.commands['race'])):
             answer = f"{answer} Entrants: {self.SRL.current_race.get_entrants_string()}"
 
-        return answer
+        return answer.strip()
 
 
 
--- xwillmarktheBot/Settings.py
+++ xwillmarktheBot/Settings.py
@@ -15,6 +15,14 @@ EDITORS = [STREAMER, 'scaramangado', 'jelster64', 'juwk']
 
 
 
+# RACES
+# ------------------
+
+# !race: in addition to SRL link, print all the current entrants
+# note that !entrants can always be used to look up the entrants
+PRINT_ENTRANTS = True
+
+
 
 
 
