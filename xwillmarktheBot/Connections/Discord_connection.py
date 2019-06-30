ffc3294ef50615b83c48fe8db2e2fda5028bfb03
xwmtp <sdstempher@ziggo.nl>
2019-06-30 18:08:11 +0200

    Adding/removing roles

--- xwillmarktheBot/Connections/Discord_connection.py
+++ xwillmarktheBot/Connections/Discord_connection.py
@@ -31,8 +31,7 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
 
-        return_message = self.message_handler.find_command(message.content.lower(), message.author)
-        logging.info('Message to send: ' + return_message)
+        return_message = self.message_handler.find_command(message.content.lower(), message.author.name)
 
         # multiple messages
         if isinstance(return_message, list):
@@ -45,3 +44,26 @@ class MyClient(discord.Client):
         if message.content == 'ping':
             await message.channel.send('pong')
 
+
+
+        if message.content.startswith('!add') or message.content.startswith('!remove'):
+            words = message.content.split(' ')
+            command = words[0]
+            if len(words) <= 1:
+                await message.channel.send('Please supply a notification role.')
+            else:
+                for word in words[1:]:
+                    role = discord.utils.get(message.guild.roles, name=word.lower())
+                    if role:
+                        try:
+                            if command == '!add':
+                                await message.author.add_roles(role)
+                                await message.channel.send("Added role '" + str(role) + "'.")
+                            if command == '!remove':
+                                await message.author.remove_roles(role)
+                                await message.channel.send("Removed role '" + str(role) + "'.")
+                        except discord.errors.Forbidden:
+                            kappa = discord.utils.get(message.guild.emojis, name='Kappa')
+                            await message.channel.send('Nice try ' + str(kappa))
+                    else:
+                        await message.channel.send('Incorrect role. Available notification roles are: stream, bingo')
